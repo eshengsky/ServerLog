e9c8dabbf6e45614e2d8bf700ec0b849994cc86f
Sky.Sun <eshengsky@163.com>
2019-10-08 14:43:11 +0800

    header name ignore case

--- chrome-extension-server-log/background.js
+++ chrome-extension-server-log/background.js
@@ -1,7 +1,7 @@
 const reqHeaderName = 'X-Request-Server-Log';
 chrome.webRequest.onBeforeSendHeaders.addListener(details => {
     // Add request headers with secret key
-    const existsServerLog = details.requestHeaders.some(header => header.name === reqHeaderName);
+    const existsServerLog = details.requestHeaders.some(header => header.name.toLowerCase() === reqHeaderName.toLowerCase());
     if (!existsServerLog) {
         if ((localStorage.serverlog_activeOn === 'on' || typeof localStorage.serverlog_activeOn === 'undefined') && localStorage.serverlog_key) {
             const keys = localStorage.serverlog_key.split(/\r?\n/).join(';');
@@ -18,7 +18,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(details => {
 const resHeaderName = 'X-Server-Log-Data';
 chrome.webRequest.onHeadersReceived.addListener(details => {
     if (localStorage.serverlog_console === 'on' || typeof localStorage.serverlog_console === 'undefined') {
-        const serverLogData = details.responseHeaders.find(header => header.name === resHeaderName);
+        const serverLogData = details.responseHeaders.find(header => header.name.toLowerCase() === resHeaderName.toLowerCase());
         if (serverLogData) {
             const logArr = JSON.parse(LZString.decompressFromEncodedURIComponent(serverLogData.value));
             chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
